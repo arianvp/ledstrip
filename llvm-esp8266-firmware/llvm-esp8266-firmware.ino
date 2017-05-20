39313c628b2d0774480cf9b3719f57422d489909
Arian van Putten <aeroboy94@gmail.com>
2017-05-20 17:23:42 +0200

    initial firmware commit

---
+++ llvm-esp8266-firmware/llvm-esp8266-firmware.ino
@@ -0,0 +1,16 @@
+#include <llvm.h>
+#include <NeoPixelBus.h>
+
+const uint16_t PixelCount = 50;
+
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
